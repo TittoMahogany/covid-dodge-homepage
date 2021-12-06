cf471b78fc90bbd7fca85eebb2819783d4ce48ba
TittoMA <tittomahogany@gmail.com>
2021-12-07 00:37:14 +0700

    OKE

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ module.exports = {
   ],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Edit Undo BRK', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
